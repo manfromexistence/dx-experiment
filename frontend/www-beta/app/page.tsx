9181085774e0a23bce717d566134543160b9406c
Harry <ajju40959@gmail.com>
2024-06-06 15:16:38 +0000

    feat: hello

--- frontend/www-beta/app/page.tsx
+++ frontend/www-beta/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="bg-background ">Hello world!!!</div>
+    <div className="flex-center bg-primary-foreground border h-32 w-1/2">Hello world!!!</div>
   );
 }
